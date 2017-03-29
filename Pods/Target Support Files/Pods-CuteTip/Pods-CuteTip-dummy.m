3cd3a8667bb8d23f330fcb5d264a3116382a0988
rawadrifai <rawad.rifai@gmail.com>
2017-03-28 19:39:18 -0700

    Renaming to prossimo

--- Pods/Target Support Files/Pods-CuteTip/Pods-CuteTip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CuteTip : NSObject
-@end
-@implementation PodsDummy_Pods_CuteTip
-@end
---
+++ Pods/Target Support Files/Pods-Prossimo/Pods-Prossimo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Prossimo : NSObject
+@end
+@implementation PodsDummy_Pods_Prossimo
+@end
